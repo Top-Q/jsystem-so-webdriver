e974f0e7010a1eb1ed16c2f3be3081eee61cb447
aharon <aharon.topq@gmail.com>
2014-01-05 11:35:45 +0200

    Refactoring started

---
+++ webdriver-so/src/main/java/org/jsystem/webdriver_so/WebDriverGenerator.java
@@ -0,0 +1,18 @@
+package org.jsystem.webdriver_so;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.android.AndroidDriver;
+
+public class WebDriverGenerator {
+	
+	protected Capabilities getCapabilities(WebDriverType webDriverType){
+		return null;
+	}
+	
+	public WebDriver getWebDriver(){
+		return null;
+		
+		
+	}
+}
